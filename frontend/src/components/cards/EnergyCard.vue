e7f9943aaaf34df0fb1b151340e2d38405a53116
Petter H. Juliussen <petter.juliussen@origo.oslo.kommune.no>
2023-02-17 19:16:32 +0100

    Add intial energy card

---
+++ frontend/src/components/cards/EnergyCard.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { useEntity } from '@/composables/entity.js';
+
+import BaseCard from './BaseCard.vue';
+
+const props = defineProps({
+  entityId: {
+    type: String,
+    required: true,
+  },
+});
+
+const entity = useEntity(props.entityId);
+
+</script>
+
+<template>
+  <BaseCard :entity="entity">
+    {{ entity }}
+  </BaseCard>
+</template>
